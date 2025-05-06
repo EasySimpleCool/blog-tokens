ffa329f0ce83438506a9fe1cc33b44851456da30
EasySimpleCool <easysimplecool@gmail.com>
2025-05-06 22:59:01 +1000

    chore: add configuration for style-dictionary and generate CSS variables for multiple themes

---
+++ config.mjs
@@ -0,0 +1,33 @@
+import { readFileSync } from 'node:fs';
+import StyleDictionary from 'style-dictionary';
+import { register, permutateThemes } from '@tokens-studio/sd-transforms';
+
+// sd-transforms, 2nd parameter for options can be added
+// See docs: https://github.com/tokens-studio/sd-transforms
+register(StyleDictionary);
+
+const $themes = JSON.parse(readFileSync('./tokens/$themes.json', 'utf-8'));
+const themes = permutateThemes($themes, { seperator: '_' });
+const configs = Object.entries(themes).map(([name, tokensets]) => ({
+  source: tokensets.map((tokenset) => `./tokens/${tokenset}.json`),
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath: 'src/css/',
+      files: [
+        {
+          destination: `_variables-${name}.css`,
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+}));
+
+for (const cfg of configs) {
+  const sd = new StyleDictionary(cfg);
+  // optionally, cleanup files first..
+  await sd.cleanAllPlatforms();
+  await sd.buildAllPlatforms();
+}
--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -3,30 +3,12 @@ import { register } from "@tokens-studio/sd-transforms";
 import StyleDictionary from "style-dictionary";
 
 // Register the Token Studio transforms properly
-register(StyleDictionary, { 
-  excludeParentKeys: false
-});
+register(StyleDictionary);
 
 // Export the configuration
 export default {
   source: [
-    'tokens/input.json',
-    'tokens/displaymode/light.json',
-    'tokens/displaymode/dark.json',
-    'tokens/brand/blue.json',
-    'tokens/brand/purple.json',
-    'tokens/brand/yellow.json',
-    'tokens/output.json',
-    'tokens/comp/box-stack.json',
-    'tokens/comp/button.json',
-    'tokens/comp/container.json',
-    'tokens/comp/image.json',
-    'tokens/comp/post-detail.json',
-    'tokens/comp/post-preview.json',
-    'tokens/comp/section.json',
-    'tokens/comp/tag.json',
-    'tokens/$metadata.json',   // For tokenSetOrder
-    'tokens/$themes.json' 
+    'tokens/**/*.json',
   ],
   // Add the preprocessor explicitly as mentioned in the docs
   preprocessors: ["tokens-studio"],
@@ -37,21 +19,18 @@ export default {
   platforms: {
     css: {
       transformGroup: "tokens-studio",
-      // transforms: ["name/kebab"],
+      transforms: ["name/kebab"],
       buildPath: "src/css/",
       files: [
         {
-          destination: "variables.css",
+          destination: "_variables-%theme%.css",
           format: "css/variables",
           options: {
-            outputReferences: true,
-            themedSelectors: {
-              "site-dark": '@media (prefers-color-scheme: dark)',
-            },
+            usesDtcg: true,
           },
         },
       ],
-      // "expand": true
+      "expand": true
     },
   },
 };
